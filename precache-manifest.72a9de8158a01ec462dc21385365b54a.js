3083ad81d4835350919280a65d68fd54f7726440
haQzz <243227413@qq.com>
2021-12-10 22:18:09 +0800

    deploy

---
+++ precache-manifest.72a9de8158a01ec462dc21385365b54a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05aa2f31649e5cb441ea",
+    "url": "/easylife-website/css/app.4e6c8632.css"
+  },
+  {
+    "revision": "1d4a8cbb4e6f7e7410429c174cf3144d",
+    "url": "/easylife-website/erweima.png"
+  },
+  {
+    "revision": "ab88cd87af44cb2731e64ce960f90084",
+    "url": "/easylife-website/index.html"
+  },
+  {
+    "revision": "05aa2f31649e5cb441ea",
+    "url": "/easylife-website/js/app.96bd7437.js"
+  },
+  {
+    "revision": "b8a63fa27e8826f3ac88",
+    "url": "/easylife-website/js/chunk-vendors.49abd244.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/easylife-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easylife-website/robots.txt"
+  }
+]);
